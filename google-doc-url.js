1c9cfa4bb2b7f82090d3cecb9b391e365db3babb
joanameta <j.meta@student.uw.edu.pl>
2017-03-25 01:04:18 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Mg12ds38dB3RwP8YrkDmhRBDilNg9tCKerHzF3FLjqI/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1oT3hru7THAyP4XM7kFdrYRqBQJfpZ64sKlI2yXF8nx4/pubhtml';
